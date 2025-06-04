4d47f7720bfa01c5185250dc3e67e60d5405aeaa
karthik <karthikchandru81@gmail.com>
2025-06-04 22:23:05 +0530

    cors fix 2

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
-app.options('*', cors());
+//app.options('*', cors());
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
